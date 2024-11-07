0777b213e36c0309d019105d0218d5e2bb38439e
Engineer-kim <ddang0103@naver.com>
2024-11-07 22:00:47 +0900

    Feature: 정적으로 작성된 데이터가 아닌 DB에서 쿼리로 가져온 데이터를 기반으로 화면구성 + 약간의 css 수정및 추가

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,12 @@ from blog.models import Author ,Post ,Tag
 
 # Register your models here.
 
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author" , "date" , "tags")
+    list_display = ("title" , "date" , "author" )
+    prepopulated_fields = {"slug": ("title",)}
 
-admin.site.register(Post)
+
+admin.site.register(Post , PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
